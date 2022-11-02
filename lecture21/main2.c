0efcb5ed0fca5d9d8a52890ef2957c11c0398f92
William Ament <will@ament.net>
2022-11-02 17:56:44 -0500

    21

---
+++ lecture21/main2.c
@@ -0,0 +1,120 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <mpi.h>
+#include <math.h>
+
+int main(int argc, char* argv[])
+{
+	// Setup MPI code
+	int comm_sz, my_rank;
+	MPI_Init(NULL, NULL);
+	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+	// Grab the global a, b, and N parameters
+	double a, b, global_T, error;
+	int N;
+	void get_input(int argc, char* argv[], const int my_rank, const int comm_sz, double* a, double* b, int* N);
+	get_input(argc, argv, my_rank, comm_sz, &a, &b, &N);
+	
+	// Get time
+	double time_start;
+	if (my_rank == 0)
+	{ time_start = MPI_Wtime(); }
+	
+	// Run local trapezoidal rule
+	const double h = (b - a) / ((double)N);
+	const int local_N = N / comm_sz;
+	const double local_a = a + ((double)(my_rank * local_N)) * h;
+	const double local_b = local_a + ((double)local_N) * h;
+	double CompTrap(const double a, const double b, const int N, const double h);
+	double local_T = CompTrap(local_a, local_b, local_N, h);
+
+	// Add local results to the global result on Processor 0
+	if (my_rank != 0)
+	{
+		MPI_Send(&local_T, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+	}
+	else
+	{
+		global_T = local_T;
+		for (int i = 1; i < comm_sz; i++)
+		{
+			MPI_Recv(&local_T, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			global_T += local_T;
+		}
+		const double Tex = exp(b) - exp(a);
+		error = fabs(Tex - global_T) / fabs(Tex);
+	}
+
+	// Print answer to screen
+	if (my_rank == 0)
+	{
+		double time_end = MPI_Wtime();
+		double time_elapsed = time_end - time_start;
+		printf(" NP = %2i, N = %i, T = %20.13e, |T-Tex|/|Tex| = %20.13e\n", comm_sz, N, global_T, error);
+		printf(" Elapsed time = %20.13e\n", time_elapsed);
+	}
+	
+	// End program
+	MPI_Finalize();
+	return 0;
+}
+
+//take in input from the user
+void get_input(int argc, char* argv[], const int my_rank, const int comm_sz, double* a, double* b, int* N)
+{
+	void usage(const char* prog_name);
+	if (my_rank == 0)
+	{
+		if (argc != 4) { usage(argv[0]); }
+		
+		*a = strtod(argv[1], NULL);
+		*b = strtod(argv[2], NULL);
+		*N = strtol(argv[3], NULL, 10);
+		 
+		if (*N <= 0) { usage(argv[0]); }
+		if (*N % comm_sz != 0) { usage(argv[0]); }
+
+		for (int i = 1; i < comm_sz; i++)
+		{
+			MPI_Send(a, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+			MPI_Send(b, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+			MPI_Send(N, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+		}
+	}
+	else
+	{
+		MPI_Recv(a, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Recv(b, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Recv(N, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	}
+}
+
+//usage message for invalid arguments
+void usage(const char* prog_name)
+{
+	fprintf(stderr, "usage: %s <a> <b> <N>\n", prog_name);
+	fprintf(stderr, " N should be positive\n");
+	fprintf(stderr, " N should be exactly divisible by the number of processors\n");
+	exit(1);
+}
+
+//compute trapazoid
+double CompTrap(const double a, const double b, const int N, const double h)
+{
+	double func(const double x);
+	double T = 0.5 * (func(a) + func(b));
+
+	for (int i = 1; i < N; i++)
+		{ T += func(a + ((double)i) * h); }
+	
+	return h * T;
+}
+
+//helper func
+double func(const double x)
+{
+	return (1.0 + exp(x));
+}
